6ab3ef8c473149cd8b471b4956a9bf2d84508ae3
Daniel Costa <daniel.costa@cartaovaibem.com.br>
2021-04-23 09:46:17 -0300

    feat: creating messages route and list

--- src/services/SettingsService.ts
+++ src/services/SettingsService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
 import { SettingsRepository } from "../repositories/SettingsRepository";
 
 interface ISettingsCreate {
@@ -7,22 +8,27 @@ interface ISettingsCreate {
 }
 
 class SettingsService {
+  private settingsRepository: Repository<Setting>;
+
+  constructor() {
+    this.settingsRepository = getCustomRepository(SettingsRepository);
+  }
+
   async create({ chat, username } : ISettingsCreate) {
-    const settingsRepository = getCustomRepository(SettingsRepository);
 
     // Select * from settings where username = "username"
-    const userAlreadyExists = await settingsRepository.findOne({username})
+    const userAlreadyExists = await this.settingsRepository.findOne({username})
 
     if (userAlreadyExists) {
       throw new Error("Usuário já existe!")
     }
 
-    const settings = settingsRepository.create({
+    const settings = this.settingsRepository.create({
       chat,
       username
     })
 
-    await settingsRepository.save(settings)
+    await this.settingsRepository.save(settings)
   }
 }
 

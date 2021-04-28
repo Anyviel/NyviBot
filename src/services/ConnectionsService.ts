c81feacbe388394e7123e01c560882b8a6018659
Daniel Costa <daniel.costa@cartaovaibem.com.br>
2021-04-28 15:05:33 -0300

    feat: Removing Clients after starting support

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -54,6 +54,16 @@ class ConnectionService {
 
     return connection;
   }
+
+  async updateAdminId(user_id: string, admin_id: string) {
+    await this.connectionsRepository.createQueryBuilder()
+    .update(Connection)
+    .set({ admin_id })
+    .where("user_id = :user_id", {
+      user_id
+    })
+    .execute();
+  }
 }
 
 export { ConnectionService }
